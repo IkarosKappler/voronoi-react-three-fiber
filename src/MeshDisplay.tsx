54984558bca18c72c8c7363f5cafb0488b14b052
ikaroskappler <ikaros.kappler@gmx.de>
2021-02-09 22:26:20 +0100

    Upgrade to plotboilerplate@1.11.1 and using Sutherland-Hodgman clipping.

--- src/MeshDisplay.tsx
+++ src/MeshDisplay.tsx
@@ -1,9 +1,11 @@
+import { Bounds } from 'plotboilerplate/src/js/Bounds';
+import { Polygon } from 'plotboilerplate/src/js/Polygon';
 import { Triangle } from 'plotboilerplate/src/js/Triangle';
 import { Delaunay } from 'plotboilerplate/src/js/utils/algorithms/delaunay';
 import { delaunay2voronoi } from 'plotboilerplate/src/js/utils/algorithms/delaunay2voronoi';
 import { VoronoiCell } from 'plotboilerplate/src/js/utils/datastructures/VoronoiCell';
 import { Vertex } from 'plotboilerplate/src/js/Vertex';
-import React, { useRef, useState } from 'react';
+import React, { useRef } from 'react';
 import { Canvas, extend, MeshProps, ReactThreeFiber, useFrame, useThree } from 'react-three-fiber';
 import { DoubleSide, Face3, Vector3 } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
@@ -41,28 +43,14 @@ declare global {
 extend({ OrbitControls })
 
 
-// interface IntrinsicElements{
-//   "group": any,
-//   "geometry": any,
-//   "lineBasicMaterial": any,
-//   "mesh": any,
-//   "octahedronGeometry": any,
-//   "meshBasicMaterial": any,
-//   "orbitControls": any, //I added this
-//   "primitive": any, //I added this
-//   "ambientLight": any //I added this
-// }
-
-
 interface VoronoiMeshProps extends MeshProps {
     voronoiDiagram : Array<VoronoiCell>;
+    clipPolygon : Polygon;
     voronoiCellScale : number;
 }
 
 const VoronoiMesh : React.FC<VoronoiMeshProps> = (props) => {
-
   const ref = useRef<Mesh>()
-
   const voronoiGeometry : VoronoiGeometry = new VoronoiGeometry( props );
   const vertices : Array<Vector3> = React.useMemo(() => voronoiGeometry.vertices.map(v => new Vector3(v.x,v.y,v.z)), [voronoiGeometry.vertices])
   const faces : Array<Face3> = React.useMemo(() => voronoiGeometry.faces.map(f => new Face3(f.a,f.b,f.c)), [voronoiGeometry.faces])
@@ -77,89 +65,63 @@ const VoronoiMesh : React.FC<VoronoiMeshProps> = (props) => {
   
 };
 
-/*
-const MyMesh: React.FC<MeshProps> = (props) => {
-  // This reference will give us direct access to the mesh
-  const mesh = useRef<Mesh>()
-
-  // Set up state for the hovered and active state
-  const [hovered, setHover] = useState(false);
-  const [active, setActive] = useState(false);
-
-  // Rotate mesh every frame, this is outside of React without overhead
-  useFrame(() => {
-    if (mesh.current) mesh.current.rotation.x = mesh.current.rotation.y += 0.01
-  })
-
-  return (
-    <mesh
-      {...props}
-      ref={mesh}
-      scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
-      onClick={(event) => setActive(!active)}
-      onPointerOver={(event) => setHover(true)}
-      onPointerOut={(event) => setHover(false)}>
-        <boxBufferGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-    </mesh>
-  )
-} */
 
 const Scene : React.FC<VoronoiMeshProps> = (props) => {
-  const {
-    camera,
-    gl: { domElement }
-  } = useThree()
-  return (
-    <>
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-      <VoronoiMesh 
-        position={[0,0,0]} 
-        scale={[1,1,1]}
-        voronoiDiagram={props.voronoiDiagram} 
-        voronoiCellScale={props.voronoiCellScale} />
-      <orbitControls args={[camera, domElement]} />
-    </>
-  )
+    const {
+	camera,
+	gl: { domElement }
+    } = useThree()
+    return (
+	<>
+	    <ambientLight />
+	    <pointLight position={[10, 10, 10]} />
+	    <VoronoiMesh 
+		position={[0,0,0]} 
+		scale={[1,1,1]}
+		voronoiDiagram={props.voronoiDiagram}
+		clipPolygon={props.clipPolygon}
+		voronoiCellScale={props.voronoiCellScale} />
+	    <orbitControls args={[camera, domElement]} />
+	</>
+    );
 }
 
+interface VoronoiPropState {
+    cells: Array<VoronoiCell>;
+    clipPolygon : Polygon;
+}
 
 export const MeshDisplay: React.FC<MeshProps> = (props) => {
 
-  const width : number = 200;
-  const height : number = 100;
+    const [ voronoiState, setVoronoiDiagram ] =
+	React.useState<VoronoiPropState>( { cells : [], clipPolygon : new Polygon([],false) } );
 
-  const [ voronoiDiagram, setVoronoiDiagram ] = React.useState<Array<VoronoiCell>>( [] );
+    React.useEffect( () => {
+	const pointList : Vertex[] = [];
+	for( var i = 0; i < 32; i++ ) {
+            pointList.push( new Vertex( -1 + Math.random()*2, 
+					-1 + Math.random()*2 
+            ));
+	}
+	const delau : Delaunay = new Delaunay( pointList );
+	const triangles : Array<Triangle>  = delau.triangulate();
 
-  React.useEffect( () => {
-     const pointList : Vertex[] = [];
-     for( var i = 0; i < 32; i++ ) {
-        pointList.push( new Vertex( Math.random(), // *width,
-                    Math.random() // *height) 
-        ));
-     }
-     const delau : Delaunay = new Delaunay( pointList );
-     const triangles : Array<Triangle>  = delau.triangulate();
+	const voronoiBuilder : delaunay2voronoi = new delaunay2voronoi(pointList,triangles);
+	const diagram : Array<VoronoiCell> = voronoiBuilder.build();
+	const clipBoxPolygon : Polygon = Bounds.computeFromVertices( pointList ).toPolygon();
 
-     var voronoiBuilder : delaunay2voronoi = new delaunay2voronoi(pointList,triangles);
-     const diagram : Array<VoronoiCell> = voronoiBuilder.build();
-     setVoronoiDiagram( diagram );
-  }, [setVoronoiDiagram] );
+	setVoronoiDiagram( { cells : diagram, clipPolygon : clipBoxPolygon } );
+    }, [setVoronoiDiagram] );
 
-  const voronoiCellScale = 0.8;
+    const voronoiCellScale = 0.8;
 
 
-  return (
-    <Canvas>
-      <Scene voronoiDiagram={voronoiDiagram} voronoiCellScale={0.8} />
-      {/* <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-      <VoronoiMesh 
-        position={[0,0,0]} 
-        voronoiDiagram={voronoiDiagram} 
-        voronoiCellScale={voronoiCellScale} /> */}
-    </Canvas>
+    return (
+	<Canvas>
+	    <Scene voronoiDiagram={voronoiState.cells}
+		   clipPolygon={voronoiState.clipPolygon}
+		   voronoiCellScale={0.8} />
+	</Canvas>
 
-  )
+    )
 }
--- src/VoronoiGeometry.ts
+++ src/VoronoiGeometry.ts
@@ -4,11 +4,16 @@
  * @requires THREE.Geometry
  * @requires VoronoiCell
  *
- * @author  Ikaros Kappler
- * @version 1.0.0
- * @date    2021-01-10
+ * @author   Ikaros Kappler
+ * @version  1.0.0
+ * @date     2021-01-10
+ * @modified 2021-02-09 Added Sutherland-Hodgman polygon clipping.
  **/
 
+import { Polygon } from 'plotboilerplate/src/js/Polygon';
+import { Vertex } from 'plotboilerplate/src/js/Vertex';
+import { XYCoords } from 'plotboilerplate/src/js/interfaces';
+import { sutherlandHodgman } from 'plotboilerplate/src/js/utils/algorithms/sutherlandHodgman';
 import { VoronoiCell } from 'plotboilerplate/src/js/utils/datastructures/VoronoiCell';
 import { Face3, Geometry, Vector3 } from 'three';
 
@@ -18,9 +23,19 @@ interface CellIndices {
 }
 
 interface VoronoiGeometryProps {
-	voronoiDiagram : Array<VoronoiCell>;
-	voronoiCellScale : number;
+    voronoiDiagram : Array<VoronoiCell>;
+    clipPolygon : Polygon;
+    voronoiCellScale : number;
 }
+
+// TODO: since version ^1.11.1 this function should be part of plotboilerplate
+const cloneVertexArray = ( vertices : Array<XYCoords> ) : Array<Vertex> => {
+    const result : Array<Vertex> = [];
+    for( var i = 0; i < vertices.length; i++ ) {
+	result.push( new Vertex(vertices[i].x, vertices[i].y) );
+    }
+    return result;
+};
 
 /**
  * @param {VoronoiCell[]} voronoiDiagram
@@ -31,37 +46,43 @@ export class VoronoiGeometry extends Geometry {
     private voronoiCellIndices : Array<CellIndices>;
     
     constructor( options : VoronoiGeometryProps ) {
+	super();
 
-		// THREE.Geometry.call( this );
-		super();
-
-		// interface CellIndices { bottomVertexIndices : number[], topVertexIndices : number[] }
-		this.voronoiCellIndices= [];
-		
-		this.buildVertices( options );
-		this.buildFaces( options );
+	// interface CellIndices { bottomVertexIndices : number[], topVertexIndices : number[] }
+	this.voronoiCellIndices= [];
+	
+	this.buildVertices( options );
+	this.buildFaces( options );
     };
 
     buildVertices( options : VoronoiGeometryProps ) {
-		var voronoiDiagram = options.voronoiDiagram;
-		for( var v in voronoiDiagram ) {
-			var cell : VoronoiCell = voronoiDiagram[v];
-			var polygon = cell.toPolygon();
-			// TODO: sharedVertex should not be private
-			polygon.scale( options.voronoiCellScale, (cell as any).sharedVertex );
+	var voronoiDiagram = options.voronoiDiagram;
+	var clipPolygon = options.clipPolygon;
+	for( var v in voronoiDiagram ) {
+	    var cell : VoronoiCell = voronoiDiagram[v];
 
-			// Create a cell
-			var bottomVertexIndices = [];
-			var topVertexIndices = [];
-			for( var i in polygon.vertices ) {
-				bottomVertexIndices.push( this.vertices.length );	
-				this.vertices.push( new Vector3( polygon.vertices[i].x, polygon.vertices[i].y, 0 ) );
-				topVertexIndices.push( this.vertices.length );
-				this.vertices.push( new Vector3( polygon.vertices[i].x, polygon.vertices[i].y, 0.1 ) ); // Height 
-			}
+	    
+	    var polygon = cell.toPolygon();
+	    
+	    // Crop too large Voronoi cells to fit inside the convex hull
+	    polygon = new Polygon(
+		cloneVertexArray(sutherlandHodgman(polygon.vertices, clipPolygon.vertices)),
+		false
+	    );
+	    polygon.scale( options.voronoiCellScale, cell.sharedVertex );
 
-			this.voronoiCellIndices.push( { bottomVertexIndices : bottomVertexIndices, topVertexIndices : topVertexIndices } );
-		}	
+	    // Create a cell
+	    var bottomVertexIndices = [];
+	    var topVertexIndices = [];
+	    for( var i in polygon.vertices ) {
+		bottomVertexIndices.push( this.vertices.length );	
+		this.vertices.push( new Vector3( polygon.vertices[i].x, polygon.vertices[i].y, 0 ) );
+		topVertexIndices.push( this.vertices.length );
+		this.vertices.push( new Vector3( polygon.vertices[i].x, polygon.vertices[i].y, 0.1 ) ); // Height 
+	    }
+
+	    this.voronoiCellIndices.push( { bottomVertexIndices : bottomVertexIndices, topVertexIndices : topVertexIndices } );
+	}	
     };
 
     buildFaces( options : VoronoiGeometryProps ) {
